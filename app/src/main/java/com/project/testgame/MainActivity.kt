b9feb5a8cf34ddb62769bf73aaa46daeffa427be
Artem <aptempp@gmail.com>
2024-04-19 00:40:04 +0300

    add demo

--- app/src/main/java/com/project/testgame/MainActivity.kt
+++ app/src/main/java/com/project/testgame/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.project.testgame
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
@@ -15,39 +14,102 @@ import com.project.gamefield.view.GameFieldView
 
 
 class MainActivity : AppCompatActivity(), GameFieldControllerInterface {
+
+    private var movingObj: GameObject? = null
+    private lateinit var gameFieldView: GameFieldView
     override fun onTouchActionDown(column: Int, row: Int) {
+        movingObj = GameFieldModel.getGameObjectAt(column, row)
     }
     override fun onTouchActionMove(column: Int, row: Int) {
     }
     override fun onTouchActionUp(column: Int, row: Int) {
+        movingObj ?: return
         val obj = GameFieldModel.getGameObjectAt(column, row)
-        if (obj == null) {
-            GameFieldModel.addGameObject(GameObject(column, row, figures[Random.nextInt(figures.size)]))
+        if (fruits.contains(movingObj!!.resourceId)) {
+            if (obj == null) {
+                GameFieldModel.removeGameObjectAt(movingObj!!.column, movingObj!!.row)
+                GameFieldModel.addGameObject(GameObject(column, row, movingObj!!.resourceId))
+            } else {
+                GameFieldModel.removeGameObjectAt(column, row)
+                GameFieldModel.removeGameObjectAt(movingObj!!.column, movingObj!!.row)
+                GameFieldModel.addGameObject(GameObject(column, row, movingObj!!.resourceId))
+                GameFieldModel.addGameObject(GameObject(movingObj!!.column, movingObj!!.row, obj.resourceId))
+            }
+            return
         }
-        else {
+        if (obj != null) {
             GameFieldModel.removeGameObjectAt(column, row)
         }
+        GameFieldModel.removeGameObjectAt(movingObj!!.column, movingObj!!.row)
+        GameFieldModel.addGameObject(GameObject(column, row, movingObj!!.resourceId))
+        movingObj = null
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.project.gamefield.R.layout.gamefield_screen)
-        val gameFieldView: GameFieldView = findViewById(com.project.gamefield.R.id.gamefield_view)
+        gameFieldView = findViewById(com.project.gamefield.R.id.gamefield_view)
         val param = gameFieldView.layoutParams
-        param.height = 1000
+        param.height = 1250
         param.width = 1000
         gameFieldView.layoutParams = param
         gameFieldView.columnCount = 8
-        gameFieldView.rowCount = 8
-        for (i in 0..8) {
-            for (j in 0..8) {
+        gameFieldView.rowCount = 10
+
+        for (i in 0..<gameFieldView.columnCount / 2) {
+            for (j in 0..<gameFieldView.rowCount) {
                 val paint = Paint()
-                paint.color = if ((i+j) % 2 == 0) Color.LTGRAY else Color.DKGRAY
+                paint.color = if ((i+j) % 2 == 0) Color.YELLOW else Color.GREEN
+                paint.alpha = 300
                 gameFieldView.setPaintCell(paint, Cell(i,j))
             }
         }
+
+        for (i in gameFieldView.columnCount/2..<gameFieldView.columnCount ) {
+            for (j in 0..<gameFieldView.rowCount ) {
+                val paint = Paint()
+                paint.color = Color.TRANSPARENT
+                paint.alpha = 180
+                gameFieldView.setPaintCell(paint, Cell(i,j))
+            }
+        }
+
+
         gameFieldView.controller = this
+        gameFieldView.setBackgroundResource(R.drawable.city)
+
+        fillChess()
+        fillFruits()
+    }
+
+
+    private fun fillFruits() {
+        for (i in gameFieldView.columnCount/2 ..<gameFieldView.columnCount) {
+            for (j in 0..<gameFieldView.rowCount) {
+                GameFieldModel.addGameObject(GameObject(i, j, fruits[Random.nextInt(fruits.size)]))
+            }
+        }
+    }
+
+    private fun fillChess() {
+        GameFieldModel.addGameObject(GameObject(0,0,R.drawable.chess_blackrook))
+        GameFieldModel.addGameObject(GameObject(1,0,R.drawable.chess_blackknight))
+        GameFieldModel.addGameObject(GameObject(2,0,R.drawable.chess_blackbishop))
+        GameFieldModel.addGameObject(GameObject(3,0,R.drawable.chess_blackqueen))
+        GameFieldModel.addGameObject(GameObject(0,gameFieldView.rowCount-1,
+            R.drawable.chess_whiterook))
+        GameFieldModel.addGameObject(GameObject(1,gameFieldView.rowCount-1,
+            R.drawable.chess_whiteknight))
+        GameFieldModel.addGameObject(GameObject(2,gameFieldView.rowCount-1,
+            R.drawable.chess_whitebishop))
+        GameFieldModel.addGameObject(GameObject(3,gameFieldView.rowCount-1,
+            R.drawable.chess_whitequeen))
+        for (i in 0..3) {
+            GameFieldModel.addGameObject(GameObject(i,1,R.drawable.chess_blackpawn))
+            GameFieldModel.addGameObject(GameObject(i,gameFieldView.rowCount-2,
+                R.drawable.chess_whitepawn))
+        }
     }
 
     private val figures: IntArray = intArrayOf(
@@ -61,5 +123,13 @@ class MainActivity : AppCompatActivity(), GameFieldControllerInterface {
         R.drawable.chess_blackbishop,
         R.drawable.chess_blackqueen,
         R.drawable.chess_blackking
+    )
+
+    private val fruits: IntArray = intArrayOf(
+        R.drawable.apple,
+        R.drawable.banana,
+        R.drawable.berry,
+        R.drawable.mango,
+        R.drawable.pineapple
     )
 }
